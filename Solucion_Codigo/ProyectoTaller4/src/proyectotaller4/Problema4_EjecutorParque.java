8a8738d48b85d10a5d04ca1112eec82ba7f5d0a6
arielcede12 <arielcede2@gmail.com>
2025-06-01 16:32:36 -0500

    a

---
+++ Solucion_Codigo/ProyectoTaller4/src/proyectotaller4/Problema4_EjecutorParque.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectotaller4;
+
+/**
+ *
+ * @author Nitro
+ */
+public class Problema4_EjecutorParque {
+    
+}
---
+++ Solucion_Codigo/ProyectoTaller4/src/proyectotaller4/ProyectoTaller4.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyectotaller4;
+
+/**
+ *
+ * @author Nitro
+ */
+public class ProyectoTaller4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
